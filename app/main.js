dc2581db74395d81b23f39562f1cdef39f7b9877
Alex Slubsky <aslubsky@gmail.com>
2016-10-13 10:37:38 +0300

    add backend

--- app/main.js
+++ app/main.js
@@ -1,10 +1,21 @@
-var mainApp = angular.module('MainApp', []);
+var mainApp = angular.module('MainApp', ['ngResource']);
 
-mainApp.controller('MainController', ['$scope', function($scope) {
-    $scope.answer = ' - ';
-
-    $scope.setAnswer = function(answer) {
-        $scope.answer = answer;
-    };
+mainApp.factory('PostModel', ['$resource', function ($resource) {
+    return $resource('http://localhost:8088/api/rest.php/posts/:id', {'id': '@id'}, {});
 }]);
 
+
+mainApp.controller('MainController', ['$scope', 'PostModel', function ($scope, PostModel) {
+    $scope.answer = ' - ';
+
+    $scope.setAnswer = function (answer) {
+        $scope.answer = answer;
+    };
+
+    $scope.getPosts = function (answer) {
+        PostModel.get(function(res) {
+            console.log('res', res.data);
+            $scope.posts = res.data;
+        });
+    };
+}]);
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,25 @@
 var gulp = require('gulp');
 var server = require('gulp-server-livereload');
+var php = require('gulp-connect-php');
 
-gulp.task('webserver', function () {
+gulp.task('webserver', ['php-api'], function () {
     gulp.src('./')
         .pipe(server({
             livereload: {
                 enable: true,
                 filter: function (filePath, cb) {
-                    cb(!(/node_modules/.test(filePath)));
-                },
+                    cb(!(/node_modules/.test(filePath)) && !(/api/.test(filePath)));
+                }
             },
             defaultFile: 'index.html',
             fallback: 'index.html',
             open: true
         }));
+});
+
+gulp.task('php-api', function () {
+    php.server({
+        port: 8088,
+        keepalive: true
+    });
 });
